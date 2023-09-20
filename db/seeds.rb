82cd5789a7cb89273324d6baa95682225de384da
Jrmcuervo <jrmcuervom@gmail.com>
2023-09-19 20:14:46 -0600

    Update seed data for the application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Message.create(greeting: "Hello World!")
+Message.create(greeting: "Good morning!")
+Message.create(greeting: "Hello again!")
+Message.create(greeting: "Greetings!")
+Message.create(greeting: "Whats up!")
